ec14af5d10eb429f0e15a253677bf9d3bd617f2e
1WXY203 <84109284+1WXY203@users.noreply.github.com>
2021-09-27 23:02:03 +0800

    Add files via upload

---
+++ huawei.js
@@ -0,0 +1,9 @@
+
+var select1 = document.getElementsByClassName('select');
+console.log(select1[0])
+function fn() {
+    alert('hhh');
+    // select1[0].setAttribute(display:block);
+    // select1[0].style.cssText ='display:block;';
+    select1[0].style.display='block' ;
+}
