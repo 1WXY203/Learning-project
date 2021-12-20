6f3f42682404f9be8d9eec020a7d7c9aea5daf64
1WXY203 <84109284+1WXY203@users.noreply.github.com>
2021-12-20 15:46:15 +0800

    Add files via upload

---
+++ vuemusic/src/api/base.js
@@ -0,0 +1,4 @@
+export let baseUrl= '/api'; //代理的地址
+export let billList = '/v1/restserver/ting?method=baidu.ting.billboard.billList';
+export let play = '/v1/restserver/ting?method=baidu.ting.song.play';
+export let lrc = '/v1/restserver/ting?method=baidu.ting.song.lry';
